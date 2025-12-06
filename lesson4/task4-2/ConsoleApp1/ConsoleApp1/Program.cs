f1b537a06799a2732a7306b37d197b0b315d3aa8
Michael <klyuchnikov.mish@gmail.com>
2025-12-06 23:17:39 +0200

    finished task 4-2

--- lesson4/task4-2/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson4/task4-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// task 4-2--------------------------------------------------------------------
+
+//user input and interface---------------------
+Console.WriteLine();
+Console.WriteLine("Enter your first number:");
+string firstInput = Console.ReadLine();
+
+Console.WriteLine("Enter your second number:");
+string secondInput = Console.ReadLine();
+//---------------------------------------------
+
+//adding a dot character to input and conversion------
+firstInput = firstInput.Replace('.', ',');
+secondInput = secondInput.Replace('.', ',');
+
+decimal firstNumber = Convert.ToDecimal(firstInput);
+decimal secondNumber = Convert.ToDecimal(secondInput);
+//----------------------------------------------------
+
+//user input and interface-----------------------
+Console.WriteLine("Enter your action: + - * / ");
+string actionSelect = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Result:");
+//-----------------------------------------------
+
+//conditions for selecting an action--------------------
+switch (actionSelect)
+{
+    case "+":
+        Console.WriteLine( firstNumber + secondNumber );
+        break;
+
+    case "-":
+        Console.WriteLine( firstNumber - secondNumber );
+        break;
+
+    case "*":
+        Console.WriteLine( firstNumber * secondNumber);
+        break;
+
+    case "/":
+        Console.WriteLine( firstNumber / secondNumber);
+        break;
+
+    default:
+        Console.WriteLine( "Incorrect action!" );
+        break;
+}
+//------------------------------------------------------

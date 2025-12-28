2d4b863e1b3f87253e51c4f4025e19d239189075
Michael <klyuchnikov.mish@gmail.com>
2025-12-28 15:39:28 +0200

    finished task9-1

--- lesson9/task9-1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson9/task9-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,38 @@
 ﻿
 
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first and last name:");
+        string input = Console.ReadLine();
+        
+        // Split the string by a space
+        string[] parts = input.Split(' ');
+        Console.WriteLine();
+
+        if (parts.Length < 2)
+        {
+            Console.WriteLine("Enter first and last name:");
+            return;
+        }
+
+        string name = parts[0];
+        string surname = parts[1];
+
+        // Bring the first letters to the same case
+        char firstNameLetter = char.ToLower(name[0]);
+        char firstSurnameLetter = char.ToLower(surname[0]);
+
+        if (firstNameLetter == firstSurnameLetter)
+        {
+            Console.WriteLine("The last name starts with the same letter as the first name.");
+        }
+        else
+        {
+            Console.WriteLine("The last name doesn't start with the same letter as the first name.");
+        }
+    }
+}

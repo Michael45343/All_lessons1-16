ed19ef9e7c96fb06686eaf24aed3f0a9a1fc37d4
Michael <klyuchnikov.mish@gmail.com>
2025-12-28 18:02:48 +0200

    finished task9-3

--- lesson9/task9-3/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson9/task9-3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,17 @@
 ﻿
 
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter string: ");
+        string input = Console.ReadLine();
+
+        string result = input.Replace(" ", "");
+
+        Console.WriteLine("Result:");
+        Console.WriteLine(result);
+    }
+}

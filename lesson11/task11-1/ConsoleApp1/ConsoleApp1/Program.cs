979b4cee90e585c4f403954258d881e23ba129fc
Michael <klyuchnikov.mish@gmail.com>
2026-01-07 10:50:43 +0200

    finished

--- lesson11/task11-1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson11/task11-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,28 @@
 ﻿
 
 
+//method
+int Fibo(int n) 
+{
+    if (n == 1)
+        return 0;
+
+    if (n == 2)
+        return 1;
+
+    int a = 0;
+    int b = 1;
+    int result = 0;
+
+    for (int i = 3; i <= n; i++)
+    {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return result;
+}
+
+//checking of the method
+Console.WriteLine(Fibo(3));

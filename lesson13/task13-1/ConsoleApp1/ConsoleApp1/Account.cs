20fd5f62787c89fe0625f47b90680e2956d574eb
Michael <klyuchnikov.mish@gmail.com>
2026-01-17 11:24:00 +0200

    1 file added

---
+++ lesson13/task13-1/ConsoleApp1/ConsoleApp1/Account.cs
@@ -0,0 +1,52 @@
+﻿
+public class Account
+{
+    private double balance;   // Closed field that cannot be changed directly
+    public string Name { get; }
+
+    // Read-only
+    public double Balance
+    {
+        get { return balance; }
+    }
+
+    public Account(string name, double initialBalance)
+    {
+        Name = name;
+
+        if (initialBalance < 0)
+            balance = 0;
+        else
+            balance = initialBalance;
+    }
+
+    // Adding money
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("The replenishment amount must be greater than zero");
+            return;
+        }
+
+        balance += amount;
+    }
+
+    // Withdrawal
+    public void Withdrawal(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("The withdrawal amount must be greater than zero");
+            return;
+        }
+
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient funds in the account...");
+            return;
+        }
+
+        balance -= amount;
+    }
+}

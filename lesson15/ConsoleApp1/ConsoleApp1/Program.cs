ae98ee1b4ed23cbdc1aff1177f4707b7cc7f0d8c
Michael <klyuchnikov.mish@gmail.com>
2026-01-22 21:08:29 +0200

    finished

--- lesson15/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson15/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
 ﻿
+using System;
+using System.IO;
 
+
+////task15
+
+class Program 
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter your path to data file: ");
+            string sourcePath = Console.ReadLine();
+
+            Console.Write("Enter your path to copy file: ");
+            string destinationPath = Console.ReadLine();
+
+            // Reading the text from data file
+            string content = File.ReadAllText(sourcePath);
+
+            // Rewriting the text to the new file
+            File.WriteAllText(destinationPath, content);
+
+            Console.WriteLine("The file was copied!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}

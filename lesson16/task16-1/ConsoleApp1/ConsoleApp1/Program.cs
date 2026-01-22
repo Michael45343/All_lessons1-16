f173ee720a56754ad286c2b713201f593da0a39e
Michael <klyuchnikov.mish@gmail.com>
2026-01-22 21:31:43 +0200

    finished

--- lesson16/task16-1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson16/task16-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
 ﻿
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter first number: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double result = a / b;
+            Console.WriteLine("Division result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: division by zero!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: incorrect number!");
+        }
+        finally
+        {
+            Console.WriteLine("Exit program.");
+        }
+    }
+}

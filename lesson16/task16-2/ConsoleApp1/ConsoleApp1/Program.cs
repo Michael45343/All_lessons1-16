09db14593d78d65272d09772d953785c5a25e392
Michael <klyuchnikov.mish@gmail.com>
2026-01-22 21:41:48 +0200

    finished

--- lesson16/task16-2/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson16/task16-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
 ﻿
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You entered the number: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: not a number!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unknown error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Exit program.");
+        }
+    }
+}
